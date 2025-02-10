439954d129185df85e75331745955fff54c94fc0
Алёна Одоевцева <constantindushin@gmail.com>
2025-02-10 15:19:03 +0300

    1.1

--- main/static/main/js/navbar.js
+++ main/static/main/js/navbar.js
@@ -1,28 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const navToggle = document.getElementById('navToggle');
-    const navLinks = document.getElementById('navLinks');
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.menu-toggle');
+    const navbarRight = document.querySelector('.navbar-right');
 
-    navToggle.addEventListener('click', () => {
-        if (navLinks.classList.contains('active')) {
-            navLinks.classList.remove('active');
-            setTimeout(() => {
-                navLinks.style.display = 'none';
-            }, 300);
-        } else {
-            navLinks.style.display = 'flex';
-            setTimeout(() => {
-                navLinks.classList.add('active');
-            }, 10);
-        }
-    });
-
-    // Обработчик события для изменения размера окна
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 768) {
-            navLinks.style.display = 'flex'; // Обеспечиваем отображение навбара на десктопах
-            navLinks.classList.remove('active');
-        } else if (!navLinks.classList.contains('active')) {
-            navLinks.style.display = 'none'; // Скрываем для мобильного вида, если неактивно
-        }
+    toggleButton.addEventListener('click', function () {
+        navbarRight.classList.toggle('active');
     });
 });
